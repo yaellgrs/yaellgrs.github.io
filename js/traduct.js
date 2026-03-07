5ec573cf24043480349769de3e0552b2db6599ff
yaellgrs <yaellegris09@gmail.com>
2026-03-08 00:44:54 +0100

    debut de la traduction en anglais

---
+++ js/traduct.js
@@ -0,0 +1,18 @@
+let currentLang = 'fr';
+
+async function loadLang(lang){
+    const response = await fetch('/json/lang.json');
+    const translations = await response.json();
+    currentLang = lang;
+
+    for(const [id, text] of Object.entries(translations[lang])){
+        const el = document.getElementById(id);
+        if(el) el.innerText = text;
+    }
+}
+
+const userLang = navigator.language.startsWith('en') ? 'en' : 'fr';
+loadLang(userLang);
+
+document.getElementById("btn-en").addEventListener('click', ()=> loadLang("en"));
+document.getElementById("btn-fr").addEventListener('click', ()=> loadLang("fr"));
